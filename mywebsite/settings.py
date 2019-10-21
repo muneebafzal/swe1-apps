ab4812a23e64a9eab8592b8f38ac23d785cfb368
Muhammad Muneeb Afzal <mma525@ADUAED05502LPMX.home>
2019-10-21 14:30:11 -0400

    Modifying settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -115,3 +115,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 #django_heroku.settings(locals())
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
+
+if "/app" in os.environ["HOME"]:
+    import django_heroku
+
+    django_heroku.settings(locals())
