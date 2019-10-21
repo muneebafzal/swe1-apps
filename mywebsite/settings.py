cea0abebb4378723d854f1af4c95cd17a337a294
Muhammad Muneeb Afzal <mma525@ADUAED05502LPMX.home>
2019-10-21 14:43:51 -0400

    Reformatting settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,11 +109,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = "/static/"
-#django_heroku.settings(locals())
+# django_heroku.settings(locals())
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
